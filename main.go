94c4e56b0118341bdfe8807bd10abcb325f1da64
Anton Haniiev <anton199906@gmail.com>
2022-06-22 14:51:20 +0300

    first

--- main.go
+++ main.go
@@ -1,16 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	arr := []int{4, 1, 4, -4, 6, 3, 8, 8}
 	var result []int
 
-	for i := 1; i < len(arr); i++ {
-		if arr[i] != arr[i-1] {
+	for i := 0; i < len(arr); i++ {
+		duplicate := false
+
+		for j := 0; j < len(result); j++ {
+			if arr[i] == result[j] {
+				duplicate = true
+				break
+			}
+		}
+		if !duplicate {
 			result = append(result, arr[i])
 		}
+
 	}
 	fmt.Println(result)
-
 }
